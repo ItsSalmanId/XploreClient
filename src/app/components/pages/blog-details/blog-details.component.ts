aa4faebb9e850ec58be4b42f0281f7badfbfd8c4
mnnandev <mnnanhavibhutta@gamil.com>
2024-10-09 10:14:15 -0700

    MB - profile medie || slider and comment section CV 0.0.12 - Client

--- src/app/components/pages/blog-details/blog-details.component.ts
+++ src/app/components/pages/blog-details/blog-details.component.ts
@@ -18,5 +18,26 @@ export class BlogDetailsComponent implements OnInit {
             backgroundImage: 'assets/img/page-title/page-title1.jpg'
         }
     ]
+    isPopupVisible: boolean = false;
+
+  // Show popup
+  showPopup() {
+    this.isPopupVisible = true;
+    setTimeout(() => {
+      const modal = document.querySelector('.custom-modal');
+      if (modal) modal.classList.add('show');
+    }, 0); // Trigger the show class after rendering
+  }
+
+  // Close popup
+  closePopup() {
+    const modal = document.querySelector('.custom-modal');
+    if (modal) {
+      modal.classList.remove('show');
+      setTimeout(() => {
+        this.isPopupVisible = false;
+      }, 500); // Delay the removal until after the transition
+    }
+  }
 
 }
